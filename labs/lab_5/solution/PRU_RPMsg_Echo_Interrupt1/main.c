71928b65bd357b864ad20c1a9ef0cc407a49a2d1
Jason Reeder <jreeder@ti.com>
2015-08-06 17:58:06 -0500

    Adding an RPMsg Echo example as lab 5
    This example will first interact with the sample client
    driver to show loopback with a Linux driver. A set of
    instructions will be provided on a wiki that will walk
    the user through modifying the example in order to use
    the RPMsg_PRU linux driver that represents itself as a
    character device in user space.
    
    Signed-off-by: Jason Reeder <jreeder@ti.com>

---
+++ labs/lab_5/solution/PRU_RPMsg_Echo_Interrupt1/main.c
@@ -0,0 +1,80 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <pru_cfg.h>
+#include <pru_intc.h>
+#include <rsc_types.h>
+#include <pru_virtqueue.h>
+#include <pru_rpmsg.h>
+#include <sys_mailbox.h>
+#include "resource_table_1.h"
+
+volatile register uint32_t __R31;
+
+/* PRU1 is mailbox module user 2 */
+#define MB_USER				2
+/* Mbox0 - mail_u2_irq (mailbox interrupt for PRU1) is Int Number 59 */
+#define MB_INT_NUMBER		59
+
+/* Host-1 Interrupt sets bit 31 in register R31 */
+#define HOST_INT			0x80000000
+
+/* The mailboxes used for RPMsg are defined in the Linux device tree
+ * PRU0 uses mailboxes 2 (From ARM) and 3 (To ARM)
+ * PRU1 uses mailboxes 4 (From ARM) and 5 (To ARM)
+ */
+#define MB_TO_ARM_HOST		5
+#define MB_FROM_ARM_HOST	4
+
+/*
+ * The name 'rpmsg-pru' corresponds to the rpmsg_pru driver found
+ * at linux-x.y.z/drivers/rpmsg/rpmsg_pru.c
+ */
+#define CHAN_NAME			"rpmsg-client-sample"
+#define CHAN_DESC			"Channel 31"
+#define CHAN_PORT			31
+
+uint8_t payload[RPMSG_BUF_SIZE];
+
+/*
+ * main.c
+ */
+void main() {
+	struct pru_rpmsg_transport transport;
+	uint16_t src, dst, len;
+
+	/* allow OCP master port access by the PRU so the PRU can read external memories */
+	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
+
+	/* clear the status of event MB_INT_NUMBER (the mailbox event) and enable the mailbox event */
+	CT_INTC.SICR_bit.STS_CLR_IDX = MB_INT_NUMBER;
+	CT_MBX.IRQ[MB_USER].ENABLE_SET |= 1 << (MB_FROM_ARM_HOST * 2);
+
+	/* Initialize pru_virtqueue corresponding to vring0 (PRU to ARM Host direction) */
+	pru_virtqueue_init(&transport.virtqueue0, &resourceTable.rpmsg_vring0, MB_TO_ARM_HOST, MB_FROM_ARM_HOST);
+
+	/* Initialize pru_virtqueue corresponding to vring1 (ARM Host to PRU direction) */
+	pru_virtqueue_init(&transport.virtqueue1, &resourceTable.rpmsg_vring1, MB_TO_ARM_HOST, MB_FROM_ARM_HOST);
+
+	/* Create the RPMsg channel between the PRU and ARM user space using the transport structure. */
+	while(pru_rpmsg_channel(RPMSG_NS_CREATE, &transport, CHAN_NAME, CHAN_DESC, CHAN_PORT) != PRU_RPMSG_SUCCESS);
+	while(1){
+		/* Check bit 31 of register R31 to see if the mailbox interrupt has occurred */
+		if(__R31 & HOST_INT){
+			/* Clear the mailbox interrupt */
+			CT_MBX.IRQ[MB_USER].STATUS_CLR |= 1 << (MB_FROM_ARM_HOST * 2);
+			/* Clear the event status, event MB_INT_NUMBER corresponds to the mailbox interrupt */
+			CT_INTC.SICR_bit.STS_CLR_IDX = MB_INT_NUMBER;
+			/* Use a while loop to read all of the current messages in the mailbox */
+			while(CT_MBX.MSGSTATUS_bit[MB_FROM_ARM_HOST].NBOFMSG > 0){
+				/* Check to see if the message corresponds to a receive event for the PRU */
+				if(CT_MBX.MESSAGE[MB_FROM_ARM_HOST] == 1){
+					/* Receive the message */
+					if(pru_rpmsg_receive(&transport, &src, &dst, payload, &len) == PRU_RPMSG_SUCCESS){
+						/* Echo the message back to the same address from which we just received */
+						pru_rpmsg_send(&transport, dst, src, payload, len);
+					}
+				}
+			}
+		}
+	}
+}
