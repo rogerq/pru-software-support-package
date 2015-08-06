6d1c85d4c0e058477e116ba97a02c8ab01a3c18d
Jason Reeder <jreeder@ti.com>
2015-08-06 16:02:49 -0500

    Removing generated lab files from tracking
    Removing all Debug and Release folders from tracking in
    the labs directory. This files are generated and can be
    rebuilt.
    
    Signed-off-by: Jason Reeder <jreeder@ti.com>

--- labs/lab_1/solution/toggle_led/Debug/main.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-main.obj: ../main.c
-main.obj: C:/temp_ccs/ccsv6/tools/compiler/ti-cgt-pru_2.1.0/include/stdint.h
-main.obj: ../../../../../include/pru_cfg.h
-
-../main.c: 
-C:/temp_ccs/ccsv6/tools/compiler/ti-cgt-pru_2.1.0/include/stdint.h: 
-../../../../../include/pru_cfg.h: 
