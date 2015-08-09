287551abc82ec6b2993dd09edf51bf6f4a61b1c6
Jason Reeder <jreeder@ti.com>
2015-08-09 16:30:56 -0500

    Added comments to the user space RPMsg echo example
    Signed-off-by: Jason Reeder <jreeder@ti.com>

--- labs/lab_5/rpmsg_pru_user_space_echo.c
+++ labs/lab_5/rpmsg_pru_user_space_echo.c
@@ -15,27 +15,39 @@ int main (void){
 	int i;
 	int result = 0;
 
+	/* Open the rpmsg_pru character device file */
 	pollfds[0].fd = open(DEVICE_NAME, O_RDWR);
 
+	/* 
+	 * If the RPMsg channel doesn't exist yet the character device 
+	 * won't either. 
+	 * Make sure the PRU firmware is loaded and that the rpmsg_pru
+	 * module is inserted. 
+	 */
 	if(pollfds[0].fd < 0) {
 		printf("Failed to open %s\n", DEVICE_NAME);
 		return -1;
 	}
 
+	/* The RPMsg channel exists and the character device is opened */
 	printf("Opened %s, sending %d messages\n\n", DEVICE_NAME, NUM_MESSAGES);
 
 	for(i = 0; i < NUM_MESSAGES; i++){
+		/* Send 'hello world!' to the PRU through the RPMsg channel */
 		result = write(pollfds[0].fd, "hello world!", 13);
 		if(result > 0)
 			printf("Message %d: Sent to PRU\n", i);			
-			
+		
+		/* Poll until we receive a message from the PRU and then print it */
 		result = read(pollfds[0].fd, readBuf, MAX_BUFFER_SIZE);
 		if(result > 0)
 			printf("Message %d received from PRU:%s\n\n", i, readBuf);
 	}
 	
+	/* Received all the messages the example is complete */
 	printf("Received %d messages, closing %s\n", NUM_MESSAGES, DEVICE_NAME);
 
+	/* Close the rpmsg_pru character device file */
 	close(pollfds[0].fd);
 
 	return 0;
